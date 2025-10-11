c6006eb36430b1bb6baa7a1b3125073d03923705
skfasi6666 <skfasi6666@gmail.com>
2025-10-11 11:59:35 -0400

    Update SearchBar.jsx

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -41,15 +41,6 @@ export default function SearchBar({ value, onChange, onSubmit, onEmptyRequest, s
             (isLg ? "h-5 w-5" : "h-4 w-4")
           }
         />
-        <button
-          onClick={handleSubmit}
-          className={
-            "absolute right-1.5 top-1/2 -translate-y-1/2 rounded-xl bg-red-600 font-medium text-white hover:bg-red-700 " +
-            (isLg ? "px-5 py-2.5 text-sm" : "px-4 py-2 text-xs")
-          }
-        >
-          Search
-        </button>
       </div>
     </motion.div>
   );
